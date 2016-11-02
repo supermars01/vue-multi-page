81415d0f649c5c750550bc0359794468a4b0dcb0
supermars01 <kings_821113@163.com>
2016-11-02 19:03:55 +0800

    second reversion

--- src/components/index/Index.vue
+++ src/components/index/Index.vue
@@ -1,12 +1,18 @@
 <template>
 	<h1>This is Index module!!!!</h1>
   <img src="../../assets/logo.png" height="200" width="200">
+  <Vfor></Vfor>
 </template>
 
-<script></script>
+<script>
+  import Vfor from "../testFolder/vfor";
+  export default {
+    name:'v',
+    components: {
+      Vfor
+    }
+  }
+</script>
 
 <style scoped>
-	h1{
-		color: rgb(5, 110, 234);
-	}
 </style>
---
+++ src/components/testFolder/vfor.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul class="one">
+    <!-- <li v-for="(item, index) of items">
+      {{ item.msg }}
+      <hr />
+    </li> -->
+    <li v-for="(index, item) of items" track-by="$index">
+         {{ item.msg }} － {{ index }}
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  name:'one',
+  data: function() {
+    return {
+      items:[
+        {msg: 'james'},
+        {msg: 'wede'},
+        {msg: 'kobe'},
+        {msg: 'antony'},
+        {msg: 'hadden'}
+      ]
+    }
+  }
+}
+</script>
+
+<style lang='sass'>
+  $color : #cacaca;
+  body{ background-color: $color;color:#999;}
+</style>
